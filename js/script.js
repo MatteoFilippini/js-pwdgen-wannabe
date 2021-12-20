8eb93ff38e1be6772a0e1dab50274a667bf6cb3d
MatteoFilippini <matteofilippini8@gmail.com>
2021-12-20 14:03:12 +0100

    added track

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+/*
+    Traccia:
+    Chiedi all’utente il suo nome,
+    poi chiedi il suo cognome,
+    poi chiedi il suo colore preferito
+    Infine scrivi sulla pagina nomecognomecolorepreferito21
+ */
+
+/*
+   
+*/
