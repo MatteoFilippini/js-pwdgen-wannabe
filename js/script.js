b2ca6f8b3aa210d0561656fbff3c154e8e37eae0
MatteoFilippini <matteofilippini8@gmail.com>
2021-12-20 14:08:10 +0100

    created road map

--- js/script.js
+++ js/script.js
@@ -9,5 +9,16 @@ console.log('JS OK');
  */
 
 /*
-   
+    1 - chiedo il nome
+    2 - salvo il nome
+    3 - chiedo il cognome
+    4 - salvo il cognome
+    5 - chiedo il colore preferito
+    6 - salvo il colore
+    7 - creo variabile password
+    8 - assegno alla variabile password l'unione del nome+cognome+colore
+    9 - aggiungo un "21" alla fine della variabile password
+    10 - creo e inizializzo un elemento del DOM
+    11 - recupero un elemento del DOM
+    12 - stampo in quell' elemento la password
 */
